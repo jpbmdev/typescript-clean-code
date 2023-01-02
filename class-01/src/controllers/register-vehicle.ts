b35952a87ed0d5f24970dd48b325fe865933ca19
jpbmdev <jpbmdev@gmail.com>
2023-01-02 17:40:51 -0500

    chore: new project structure created

---
+++ class-01/src/controllers/register-vehicle.ts
@@ -0,0 +1,17 @@
+export class RegisterVehicle {
+  handle(httpRequest: any): any {
+    if (!httpRequest?.body?.name) {
+      return {
+        statusCode: 400,
+        body: new Error("error in the name"),
+      };
+    }
+
+    if (!httpRequest?.body?.model) {
+      return {
+        statusCode: 400,
+        body: new Error("error in the model"),
+      };
+    }
+  }
+}
--- class-01/src/test.ts
+++
@@ -1,5 +0,0 @@
-function test(): number {
-  return 23;
-}
-
-test();
