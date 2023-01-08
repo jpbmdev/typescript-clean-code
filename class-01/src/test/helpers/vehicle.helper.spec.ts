428920ca52050155a18a8d344cf503326d8b07b6
jpbmdev <jpbmdev@gmail.com>
2023-01-08 17:34:10 -0500

    test: Vehicle Helper tests created

---
+++ class-01/src/test/helpers/vehicle.helper.spec.ts
@@ -0,0 +1,8 @@
+import { VehicleHelper } from "../../helpers/vehicle.helper";
+
+describe("Food Helper", () => {
+  test("Food Helper", () => {
+    const vehicleHelper = new VehicleHelper();
+    expect(vehicleHelper).toEqual(vehicleHelper);
+  });
+});
