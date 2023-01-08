0145f09cf0504e0ecfe4469fb25835bd8693658a
jpbmdev <jpbmdev@gmail.com>
2023-01-08 15:46:22 -0500

    refactor: New Missing parameter custom error created

--- class-01/src/test/controller/register-vehicle.spec.ts
+++ class-01/src/test/controller/register-vehicle.spec.ts
@@ -1,4 +1,5 @@
 import { RegisterVehicle } from "../../controllers/register-vehicle";
+import { MissingFormalParameter } from "../../errors/client.error";
 
 describe("RegisterVehicle", () => {
   test("if the name does not exists return 400", () => {
@@ -13,7 +14,7 @@ describe("RegisterVehicle", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("error in the name"));
+    expect(httpResponse.body).toEqual(new MissingFormalParameter("name"));
   });
 
   test("if the model does not exists return 400", () => {
@@ -28,7 +29,7 @@ describe("RegisterVehicle", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("error in the model"));
+    expect(httpResponse.body).toEqual(new MissingFormalParameter("model"));
   });
 
   test("if the model does not exists return 400", () => {
@@ -42,7 +43,7 @@ describe("RegisterVehicle", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("error in the year"));
+    expect(httpResponse.body).toEqual(new MissingFormalParameter("year"));
   });
 
   test("if the color does not exists return 400", () => {
@@ -57,6 +58,6 @@ describe("RegisterVehicle", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("error in the color"));
+    expect(httpResponse.body).toEqual(new MissingFormalParameter("color"));
   });
 });
