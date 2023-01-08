0cafa777c053c517b11255165c3092b78f084f89
jpbmdev <jpbmdev@gmail.com>
2023-01-08 14:42:49 -0500

    refactor: VehicleHelper now injected in VehicleModel

--- class-01/src/test/model/vehicle.model.spec.ts
+++ class-01/src/test/model/vehicle.model.spec.ts
@@ -2,10 +2,18 @@ import { VehicleModel } from "../../models/vehicle.model";
 
 describe("Classes", () => {
   it("Class VehicleModel", () => {
-    const vehicleModel = new VehicleModel("Nissan", "DXT", 2020);
+    const vehicleModel = new VehicleModel({
+      name: "Nissan",
+      model: "DXT",
+      year: 2020,
+      price: 25,
+      inventory: true,
+    });
 
     expect(vehicleModel.getName()).toEqual("Nissan");
     expect(vehicleModel.getModel()).toEqual("DXT");
     expect(vehicleModel.getYear()).toEqual(2020);
+    expect(vehicleModel.getPrice()).toEqual(25);
+    expect(vehicleModel.getInventory()).toEqual(true);
   });
 });
